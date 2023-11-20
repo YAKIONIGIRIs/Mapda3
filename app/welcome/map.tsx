620967d1c170f69774c9a09d9525d06028b6ba56
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-11-20 18:59:21 +0000

    経路を描画するマップ

---
+++ app/welcome/map.tsx
@@ -0,0 +1,46 @@
+// "use client";
+import React from "react";
+import { GoogleMap, LoadScript, PolylineF } from "@react-google-maps/api";
+import { mapStyle } from "./mapConfig";
+
+interface MyGoogleMapProps {
+  path: { lat: number; lng: number }[];
+}
+
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "";
+
+const containerStyle = {
+  width: "100%",
+  height: "80vh",
+  borderRadius: "10px",
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+};
+
+const center = {
+  lat: 35.6653282,
+  lng: 139.73645059999998,
+};
+
+const MyGoogleMapComponent: React.FC<MyGoogleMapProps> = ({ path }) => {
+  return (
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+        options={{ styles: mapStyle }}
+      >
+        <PolylineF
+          path={path}
+          options={{
+            strokeColor: "#FFFF01",
+            strokeOpacity: 0.6,
+            strokeWeight: 8,
+          }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default MyGoogleMapComponent;
