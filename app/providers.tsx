b6c287a833b04c40ac8e5723e80ece95f2e4bc51
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-11-21 09:11:30 +0000

    Thirdweb providerを追加

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,15 +5,36 @@ import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  localWallet,
+  embeddedWallet,
+} from "@thirdweb-dev/react";
+
 export interface ProvidersProps {
-	children: React.ReactNode;
-	themeProps?: ThemeProviderProps;
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-	return (
-		<NextUIProvider>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider>
+      <ThirdwebProvider
+        activeChain="mumbai"
+        clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+        supportedWallets={[
+          metamaskWallet(),
+          localWallet(),
+          embeddedWallet({
+            auth: {
+              options: ["email", "google", "facebook"],
+            },
+          }),
+        ]}
+      >
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </ThirdwebProvider>
+    </NextUIProvider>
+  );
 }
