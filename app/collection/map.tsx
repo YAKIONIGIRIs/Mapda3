dfb6741b5967879a6f160b299f0b93c9aa7c7129
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-11-24 21:50:44 +0000

    コレクションページを作成（パノラマ画像も表示）

---
+++ app/collection/map.tsx
@@ -0,0 +1,42 @@
+// "use client";
+import React from "react";
+import { GoogleMap, LoadScript, PolylineF } from "@react-google-maps/api";
+import { mapStyle } from "./mapConfig";
+
+interface MyGoogleMapProps {
+  path: { lat: number; lng: number }[];
+  center: { lat: number; lng: number };
+}
+
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "";
+
+const containerStyle = {
+  width: "100%",
+  height: "80vh",
+  borderRadius: "10px",
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+};
+
+const MyGoogleMapComponent: React.FC<MyGoogleMapProps> = ({ path, center }) => {
+  return (
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+        options={{ styles: mapStyle }}
+      >
+        <PolylineF
+          path={path}
+          options={{
+            strokeColor: "#FF8C00",
+            strokeOpacity: 0.8,
+            strokeWeight: 8,
+          }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default MyGoogleMapComponent;
